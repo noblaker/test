00470290e64e1e2bf758ff05114b9aaee9e8ef0b
noblaker <923541921@qq.com>
2021-11-24 17:15:27 +0800

    added a automation py script

---
+++ baidu.py
@@ -0,0 +1,9 @@
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get('https://baidu.com')
+
+driver.find_element_by_id('kw').send_keys('Selenium2')
+driver.find_element_by_id('su').click()
+driver.quit()
