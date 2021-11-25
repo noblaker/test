4c74947259987e9ba2443d116c47b4df3df8441c
noblaker <923541921@qq.com>
2021-11-25 08:07:11 +0800

    add a funtion that the web page can suspend for a while

--- baidu.py
+++ baidu.py
@@ -1,9 +1,11 @@
 
 from selenium import webdriver
+import time
 
 driver = webdriver.Firefox()
 driver.get('https://baidu.com')
 
 driver.find_element_by_id('kw').send_keys('Selenium2')
 driver.find_element_by_id('su').click()
+time.sleep(2)
 driver.quit()
